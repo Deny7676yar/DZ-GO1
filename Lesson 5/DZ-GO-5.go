50916388372b53c93f876775146c8f6c896d1740
Deny7676 <komarov7676@yandex.ru>
2021-06-21 00:12:48 +0300

    Lesson5

---
+++ Lesson 5/DZ-GO-5.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func fibonachi(n int, mapFib map[int]int) int {
+
+	fibvalue, ok := mapFib[n]
+	if ok{
+		return mapFib[n]
+	}
+	if !ok {
+		fmt.Printf("%d\tneed to be calculated\n", n)
+		fibvalue = fibonachi(n - 1, mapFib)+ fibonachi(n - 2, mapFib)
+	}
+
+	mapFib[n] = fibvalue
+	return mapFib[n]
+}
+
+
+func main () {
+	var mapFib = map[int]int{0: 0, 1: 1, 2: 1}
+	var n int
+
+	fmt.Fscanln(os.Stdin, &n)
+	fib := fibonachi(n, mapFib)
+
+	fmt.Println(fib)
+
+}
